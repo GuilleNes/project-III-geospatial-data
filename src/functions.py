022942cec5f4f99ebd654b6dd170b1a3d93995a0
Guillermo Nespral <gnsg32@gmail.com>
2022-10-30 19:10:36 +0100

    uploaded project

--- src/functions.py
+++ src/functions.py
@@ -1,3 +1,12 @@
+import os
+import requests
+import json
+from dotenv import load_dotenv
+import pandas as pd
+import pymongo
+import geopandas as gpd
+from cartoframes.viz import Map, Layer, popup_element
+import geopy
 
 # We create a function to collect data from the offices column(country code, address, lat&long...).
 
@@ -34,11 +43,64 @@ def value_counts(df_,newcolumn, counting):
 # Keeping only the columns with certain values
 
 def keep_value(dfra, column, value):
-    dfra = dfra[(dfra[column] > value)]
+    dfra = dfra[(dfra[column] >= value)]
     dfra.reset_index(drop=True, inplace=True)
     return dfra
 
+#----------------------------------------------------
+
+# Getting results from foursquare through the API
+
+
+def get_results(query, location, limit, radius, token):
+    ll = f"{location[0]}%2C{location[1]}"   
+    url = f"https://api.foursquare.com/v3/places/search?query={query}&ll={ll}&limit={str(limit)}&radius={radius}"
+    headers = {
+        "accept": "application/json",
+        "Authorization": token,
+    }
+    response = requests.get(url, headers=headers).json()
+    return response
+
+
+# We clean the results from the API keeping only the values that we need
+
+def clean_results_first(df_, categ):
+    new_list = []
+    for i in df_["results"]:
+        name = i["name"]
+        address =  i["location"]["formatted_address"]
+        lat = i["geocodes"]["main"]["latitude"]
+        lon = i["geocodes"]["main"]["longitude"]
+        neighborhood = i["location"]['neighborhood'][0]   
+        new_list.append({"name":name,"category": categ,  "address": address, 'neighborhood': neighborhood, "lat":lat, "lon":lon})        
+    return pd.DataFrame(new_list)
 
 
 
+def clean_results(df_, dfr, categ):
+    new_list = []
+    for i in df_["results"]:
+        name = i["name"]
+        address =  i["location"]["formatted_address"]
+        lat = i["geocodes"]["main"]["latitude"]
+        lon = i["geocodes"]["main"]["longitude"]
+        neighborhood = i["location"]['neighborhood'][0]   
+        new_list.append({"name":name, "category": categ, "address": address, 'neighborhood': neighborhood, "lat":lat, "lon":lon})
+    df = pd.DataFrame(new_list)
+    return pd.concat([dfr, df], axis=0).reset_index(drop= True)
+
+def add_company(dfr, address, neigh, lat, lon):
+    new_row = pd.DataFrame({'name': "Company",'address': address,'neighborhood': neigh,'lat': lat,"lon": lon}, index =[0])
+    return pd.concat([new_row, dfr]).reset_index(drop = True)
+
+
+# Getting the distance from any point to the company office in meters
+
+def calc_distance_SF(x):
+    site_coords = (37.7871253, -122.4015358)
+    place2_coords = (x.lat, x.lon)
+    return (geopy.distance.geodesic(site_coords, place2_coords).km)*1000
+
+# df_SF["Distance"] = df_SF.apply(calc_distance_SF, axis = 1).round(2)
         
